47fab52df58788a557c99ee6878122c6b8a345df
IQMETRIXHO\Kevin.Quing <kevinq@iqmetrix.com>
2016-09-15 14:07:21 +0800

    updates and configs -updated typings -body-parser, typings and module -include vscode configuration

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/body-parser/index.d.ts" />
 /// <reference path="modules/chai/index.d.ts" />
 /// <reference path="modules/debug/index.d.ts" />
 /// <reference path="modules/express/index.d.ts" />
